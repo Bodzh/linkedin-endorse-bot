701bbab908f2d442845173a755ddbe591fdbfe50
bodzh <bodzhe@gmail.com>
2019-04-24 23:11:19 +0300

    initial commit. Added script file, updated readme.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # linkedin-endorse-bot
 auto - endorsing script for linkedin profile page
+
+just copy script from script.js file, open your contact's profile page on linkedin.com, press ctr+shift+I , then paste script to console, and press ENTER.
---
+++ script.js
@@ -0,0 +1,68 @@
+scrollPageToBottom().then(function () {
+    scrollPageToMiddle().then(function () {
+        $('.pv-skills-section__additional-skills').click();
+
+        setTimeout(function(){
+            var endorses = $('.pv-skill-entity__featured-endorse-button-shared');
+            console.log('endorses available', endorses.length);
+
+            for (var i=0; i<endorses.length; i++) {
+                endorses[i].click();
+
+
+                clickVeryGood().then(function () {
+                    clickSubmit();
+                })
+            }
+
+            console.log('all done');
+        },1500);
+    })
+});
+
+function clickVeryGood() {
+    return new Promise(function(resolve) {
+        var verugoodbtn = $('#endorsement-followup-proficiency-1-Performance');
+
+        verugoodbtn.click();
+
+        setTimeout(function () {
+            resolve(true);
+        },1500)
+    });
+}
+
+function clickSubmit () {
+    return new Promise(function(resolve) {
+        var submitBtn = $('.pv-endorsement-followup__footer > button');
+
+        submitBtn.click();
+
+        setTimeout(function () {
+            resolve(true);
+        },1500)
+    })
+}
+
+function scrollPageToMiddle () {
+    return new Promise(function (resolve) {
+        let thirdOfHeight = Math.floor(document.body.clientHeight/3);
+
+        scroll(0, thirdOfHeight);
+
+
+        setTimeout(function () {
+            resolve(true);
+        },1500)
+    });
+}
+
+function scrollPageToBottom () {
+    return new Promise(function (resolve) {
+        scroll(0, document.body.clientHeight);
+
+        setTimeout(function () {
+            resolve(true);
+        },1500)
+    });
+}
